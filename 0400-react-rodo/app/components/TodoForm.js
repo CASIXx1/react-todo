9298f96e754d52d603de63b08c142ba6182039c9
CASIXx1 <casix.work@gmail.com>
2026-03-13 00:23:33 +0900

    TodoForm CSS module作成

--- 0400-react-rodo/app/components/TodoForm.js
+++ 0400-react-rodo/app/components/TodoForm.js
@@ -1,38 +1,32 @@
+import styles from "./TodoForm.module.sass";
+
 export default function TodoForm() {
   return (
-    <form className="form js-form">
-      <div className="form__input-group">
-        <div className="form__input form__input--name">
-          <label className="form__input-label" htmlFor="name">
-            タスク
-          </label>
+    <form className={styles.form}>
+      <div className={styles["form__input-group"]}>
+        <div
+          className={`${styles["form__input"]} ${styles["form__input--name"]}`}
+        >
+          <label className={styles["form__input-label"]}>タスク</label>
           <input
-            id="name"
             name="name"
             type="text"
-            className="form__input-field"
+            className={styles["form__input-field"]}
             placeholder="タスク名を入力"
           />
         </div>
-
-        <div className="form__input">
-          <label className="form__input-label" htmlFor="deadline">
-            期限日
-          </label>
+        <div className={styles["form__input"]}>
+          <label className={styles["form__input-label"]}>期限日</label>
           <input
-            id="deadline"
             name="deadline"
             type="date"
-            className="form__input-field"
+            className={styles["form__input-field"]}
             placeholder="期限日を入力"
           />
         </div>
       </div>
-
-      <div className="form__input-footer">
-        <button type="submit" className="button button--primary">
-          追加
-        </button>
+      <div className={styles["form__input-footer"]}>
+        <button className="button button--primary">追加</button>
       </div>
     </form>
   );
